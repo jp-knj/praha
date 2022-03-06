e3a161d9f87d066aa81a58d489759fac3cfeac88
jp_knj <kenji.tomita1996@gmail.com>
2022-03-06 20:15:59 +0900

    :+1: remove the danger of as

--- test/01_jestで単体テストを書こう/implementation/nameApiService.ts
+++ test/01_jestで単体テストを書こう/implementation/nameApiService.ts
@@ -8,7 +8,7 @@ export class NameApiService {
     const { data } = await axios.get(
       "https://random-data-api.com/api/name/random_name"
     );
-    const firstName = data.first_name as string;
+    const firstName:string = data.first_name;
 
     if (firstName.length > this.MAX_LENGTH) {
       throw new Error("firstName is too long!");
