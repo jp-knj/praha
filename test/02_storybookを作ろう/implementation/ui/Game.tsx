14422a94d14a25d3b4e4b2ca761fc77dcfd0ea78
jp_knj <kenji.tomita1996@gmail.com>
2022-03-13 18:49:21 +0900

    :construction: create story each component

---
+++ test/02_storybookを作ろう/implementation/ui/Game.tsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Home.module.css'
+
+import Board from "../ui/Board"
+
+function Game() {
+    return (
+        <main className={styles.main}>
+            <Board />
+        </main>
+        )
+}
+export default Game;
