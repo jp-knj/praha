cebda197745462d001db00c90617807841b930e3
jp_knj <kenji.tomita1996@gmail.com>
2022-03-13 14:50:20 +0900

    :art: show circle and cross if clicked

--- test/02_storybookを作ろう/implementation/containers/Board.tsx
+++ test/02_storybookを作ろう/implementation/containers/Board.tsx
@@ -1,4 +1,43 @@
+import {useState} from 'react';
+
+import Square from '../ui/Square'
+
 function Board(){
-    return <div>This is the board</div>
+    const [squares, setSquares] = useState(Array(9).fill(null))
+    const [winner, setWinner] = useState(null)
+    const [currentPlayer, setCurrentPlayer] = useState<'X'| 'O'>(
+        Math.round(Math.random()* 1) === 1 ? 'X': 'O'
+    )
+    function setSquareValue(index: number){
+        const newData: unknown[] = squares.map((val, i) => {
+            if(i === index){
+                return currentPlayer;
+            }
+            return val;
+        });
+        setSquares(newData)
+        setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X')
+    }
+    return (
+        <div>
+            <p>Hey {currentPlayer}, its your turn</p>
+            {Array(9)
+                .fill(null)
+                .map((_, i) => {
+                return (
+                    <Square
+                        key={i}
+                        winner={winner}
+                        onClick={()=> setSquareValue(i)}
+                        value={squares[i]}
+                />)
+            })
+            }
+        </div>
+    )
 }
 export default Board;
+
+function i(_: any, i: any): import("react").ReactNode {
+    throw new Error('Function not implemented.');
+}
--- test/02_storybookを作ろう/implementation/ui/Square.tsx
+++ test/02_storybookを作ろう/implementation/ui/Square.tsx
@@ -1,5 +1,14 @@
-function Square(){
-    return <div>square</div>
+type Player = 'X'| 'O' | null
+
+type Props = {
+    winner: Player
+    value: Player
+    onClick: () => void;
+}
+
+function Square({onClick, value, winner}:Props):JSX.Element {
+    const square = !value ? <button onClick={onClick} disabled={Boolean(winner)} /> : <button>{value}</button>;
+    return square;
 }
 
 export default Square;
