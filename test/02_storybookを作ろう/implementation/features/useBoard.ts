4f1fcc575d9115033c2d8c44cd7b13506da4de08
jp_knj <kenji.tomita1996@gmail.com>
2022-03-15 21:08:22 +0900

    :sparkles: do SoC

---
+++ test/02_storybookを作ろう/implementation/features/useBoard.ts
@@ -0,0 +1,78 @@
+import { useEffect, useState} from 'react'
+import { Player } from '../models/Player'
+
+export type UseBoardResult = {
+  winner: Player
+  squares: Player[]
+  currentPlayer: "X"|"O"
+  calculateWinner: (squares: Player[]) => Player|null
+  setSquareValue:(index: number) => void
+  resetTicTacToe: () => void
+}
+export const useBoard = ():UseBoardResult => {
+  const [winner, setWinner] = useState<Player>(null);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
+    Math.round(Math.random() * 1) === 1 ? "X" : "O"
+  );
+  const calculateWinner = (squares: Player[]):Player|null => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const setSquareValue = (index: number) => {
+    const newData: unknown[] = squares.map((val, i) => {
+      if (i === index) {
+        return currentPlayer;
+      }
+      return val;
+    });
+    setSquares(newData);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const resetTicTacToe = () => {
+    setSquares(Array(9).fill(null));
+    setWinner(null);
+    setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
+  };
+
+  useEffect(() => {
+    const w = calculateWinner(squares);
+    if (w) {
+      setWinner(w);
+    }
+
+    if (!w && !squares.filter((square) => !square).length) {
+      setWinner("BOTH");
+    }
+  });
+
+  return {
+    winner,
+    squares,
+    currentPlayer,
+    calculateWinner,
+    setSquareValue,
+    resetTicTacToe
+  }
+}
--- test/02_storybookを作ろう/implementation/ui/Board.tsx
+++ test/02_storybookを作ろう/implementation/ui/Board.tsx
@@ -1,68 +1,9 @@
-import { useEffect, useState } from "react";
-
 import { Player } from "../models/Player";
 import Button from "../ui/Button";
 import Square from "../ui/Square";
-
+import {useBoard } from "../features/useBoard";
 function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
-    Math.round(Math.random() * 1) === 1 ? "X" : "O"
-  );
-  const [winner, setWinner] = useState<Player>(null);
-
-  const setSquareValue = (index: number) => {
-    const newData: unknown[] = squares.map((val, i) => {
-      if (i === index) {
-        return currentPlayer;
-      }
-      return val;
-    });
-    setSquares(newData);
-    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
-  };
-
-  const calculateWinner = (squares: Player[]) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const resetTicTacToe = () => {
-    setSquares(Array(9).fill(null));
-    setWinner(null);
-    setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
-  };
-
-  useEffect(() => {
-    const w = calculateWinner(squares);
-    if (w) {
-      setWinner(w);
-    }
-
-    if (!w && !squares.filter((square) => !square).length) {
-      setWinner("BOTH");
-    }
-  });
-
+  const { winner, squares, currentPlayer, calculateWinner, setSquareValue, resetTicTacToe } = useBoard();
   return (
     <div>
       {!winner && <p>Hey {currentPlayer}, it's your turn</p>}
