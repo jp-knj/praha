8f18dcdb3a70b902b1c56ac9dbf86ebfd64787d0
jp_knj <kenji.tomita1996@gmail.com>
2022-03-27 21:53:17 +0900

    :art: update game components

--- test/02_storybookを作ろう/implementation/ui/Game/Game.tsx
+++ test/02_storybookを作ろう/implementation/ui/Game/Game.tsx
@@ -1,6 +1,11 @@
 import {Board} from "../Board/Board";
+import { useBoard } from '../Board/useBoard';
 
-function Game() {
-  return <Board />;
+const Game = () => {
+  const initialValues = Array(9).fill(null);
+  const initialPlayer = 'O';
+  const initialWinner = '';
+  const props = useBoard({initialValues, initialPlayer, initialWinner});
+  return <Board  {...props}/>;
 }
 export default Game;
