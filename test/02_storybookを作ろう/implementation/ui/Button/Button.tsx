8e93aafd30e68ce5c1ea47a68110c19b5ba5671f
jp_knj <kenji.tomita1996@gmail.com>
2022-03-27 18:40:25 +0900

    :construction: structure redir

---
+++ test/02_storybookを作ろう/implementation/ui/Button/Button.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  onClick: () => void;
+};
+
+function Button({ onClick }: Props): JSX.Element {
+  return (
+    <button className="reset" onClick={onClick}>
+      Reset
+    </button>
+  );
+}
+
+export default Button;
