a3273e0d9e67da7c9beaaf39f6cfb0d2ca080eec
jp_knj <kenji.tomita1996@gmail.com>
2022-11-15 21:39:36 +0900

    :tada: done task

--- 06_WEBの基礎/05_サードパーティクッキーについて理解する/implement/index.js
+++ 06_WEBの基礎/05_サードパーティクッキーについて理解する/implement/index.js
@@ -3,12 +3,26 @@ import express from 'express';
 const app = express();
 
 app.get("/", (request, response) =>{
-  response.cookie("domain origin", request.hostname,{
+  response.cookie("example first domain", request.hostname,{
     httpOnly: true
   });
   response.sendFile(__dirname + 'index.html')
 })
 
+app.use(express.static('cat',{
+  setHeaders: (res,path, stat) => {
+    res.cookie('example second domain','', {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true
+    })
+  }
+}))
+
 app.listen(3000, () => {
-  console.log(`server start localhost:${3000}`)
+  console.log('server:localhost:3000')
+})
+
+app.listen(3001, () => {
+  console.log('server:localhost:3001')
 })
